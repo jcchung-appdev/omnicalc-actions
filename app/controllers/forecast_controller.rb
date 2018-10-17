1b91c3843fdc68935194a7ec3ffc5588e480a9e2
JC Chung <jcchung@chicagobooth.edu>
2018-10-17 04:06:38 +0000

    Weather

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -10,16 +10,18 @@ class ForecastController < ApplicationController
     # The latitude the user input is in the string @lat.
     # The longitude the user input is in the string @lng.
     # ==========================================================================
+    url = "https://api.darksky.net/forecast/"+ENV.fetch("DARKSKY_KEY")+"/"+@lat+","+@lng
+    data = JSON.parse(open(url).read)
+    
+    @current_temperature = data.dig("currently","temperature")
 
-    @current_temperature = "Replace this string with your answer"
+    @current_summary = data.dig("currently","summary")
 
-    @current_summary = "Replace this string with your answer"
+    @summary_of_next_sixty_minutes = data.dig("minutely", "summary")
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer"
+    @summary_of_next_several_hours = data.dig("hourly", "summary")
 
-    @summary_of_next_several_hours = "Replace this string with your answer"
-
-    @summary_of_next_several_days = "Replace this string with your answer"
+    @summary_of_next_several_days = data.dig("daily", "summary")
 
     render("forecast_templates/coords_to_weather.html.erb")
   end
--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -11,16 +11,26 @@ class MeteorologistController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
+    
+    geo_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+url_safe_street_address+"&key="+ENV.fetch("GMAPS_KEY")
+    geo_data = JSON.parse(open(geo_url).read)
+    
+    lat = geo_data.dig("results",0,"geometry", "location", "lat").to_s
 
-    @current_temperature = "Replace this string with your answer"
+    lng = geo_data.dig("results",0,"geometry", "location", "lng").to_s
+    
+    url = "https://api.darksky.net/forecast/"+ENV.fetch("DARKSKY_KEY")+"/"+lat+","+lng
+    data = JSON.parse(open(url).read)
+    
+    @current_temperature = data.dig("currently","temperature")
 
-    @current_summary = "Replace this string with your answer"
+    @current_summary = data.dig("currently","summary")
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer"
+    @summary_of_next_sixty_minutes = data.dig("minutely", "summary")
 
-    @summary_of_next_several_hours = "Replace this string with your answer"
+    @summary_of_next_several_hours = data.dig("hourly", "summary")
 
-    @summary_of_next_several_days = "Replace this string with your answer"
+    @summary_of_next_several_days = data.dig("daily", "summary")
 
     render("meteorologist_templates/street_to_weather.html.erb")
   end
